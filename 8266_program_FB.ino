4724b2b933d9c24df53d0e739000435c5b56dd2a
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-02-28 22:07:36 -0400

    Se muestra ESP, Fase PRE-EPROM

--- 8266_program_FB.ino
+++ 8266_program_FB.ino
@@ -1,5 +1,10 @@
+//Libreries
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+
+//Information about this ESP8266
+#define ID "eyJuYW1lIjoiTU9EMDAwLUVTUDAwMCIsImlhdCI6MX0"
+#define ESP_NAME "MOD000-ESP000"
 
 // Set these to run example.
 #define FIREBASE_HOST "final-project-web-v0-default-rtdb.firebaseio.com" //Host of database
@@ -17,7 +22,25 @@
 #define D6 12 //D6 of ESP8266
 #define D7 13 //D7 of ESP8266
 
-// LED tiene logica negativa 
+boolean ValuesOfBits[8] = {true, false, true, false, true, false, true, false}; //Este arreglo debe ser guardado en memoria, Es el estado de los datos.
+
+void esp_presets(){ //Funcion que configura el nodo de el ESP.
+  Firebase.set("MOD000-ESP000/ID", ID); //Establece su ID
+  
+  for(int i = 0; i <= 7; i++){ // Loop que hace que se creen todos los bits y se agregen su valor guardado en la EPROM
+    Firebase.set(String(ESP_NAME) + "/B" + String(i), ValuesOfBits[i]);
+    }
+    
+    for(int i = 0; i < 7; i++){ //Imprime por consola el estado de los BITS
+      Serial.println(String(ValuesOfBits[i]));
+    }
+    
+   if (Firebase.failed()) {
+      Serial.print("pushing /logs failed:");
+      Serial.println(Firebase.error());  
+      return;
+   }
+}
 
 void setup() {
    Serial.begin(9600);
@@ -36,28 +59,26 @@ void setup() {
   Firebase.begin(FIREBASE_HOST);
 
   pinMode(D4, OUTPUT);
-}
 
+
+  //Esta era una logica para hacer que no se repitieran los datos, pero la reemplace
+  //if(){
+    //Serial.println("ESP: " + String(ESP_NAME) + " already exist...");
+    //Serial.println(Firebase.getBool(ESP_NAME));
+    //} else {
+        esp_presets();
+        //Serial.println(Firebase.getBool(ESP_NAME));
+      //}
+}
 int n = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // set value
-
-  if(Firebase.getBool("led")){
-     digitalWrite(D4, LOW);
-    } else {
-      digitalWrite(D4, HIGH);
-      }
-  
-  //Firebase.setFloat("number", 42.0);
-  // handle error
-  //if (Firebase.failed()) {
-      //Serial.print("setting /number failed:");
-      //Serial.println(Firebase.error());  
-      //return;
-  //}
-  //delay(1000);
+  //if(Firebase.getBool("led/")){
+     //digitalWrite(D4, LOW);
+    //} else {
+      //digitalWrite(D4, HIGH);
+      //}
+      
   
   // update value
   //Firebase.setFloat("number", 43.0);
@@ -98,14 +119,7 @@ void loop() {
   //}
   //delay(1000);
 
-  // append a new value to /logs
-  //String name = Firebase.pushInt("logs", n++);
-  // handle error
-  //if (Firebase.failed()) {
-      //Serial.print("pushing /logs failed:");
-      //Serial.println(Firebase.error());  
-      //return;
-  //}
+  
   //Serial.print("pushed: /logs/");
   //Serial.println(name);
   
